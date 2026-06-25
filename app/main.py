b30575026fdc25a3676e8e317f54540a66a398b7
muhammads-git <roadprogrammar@gmail.com>
2026-06-25 11:52:59 +0500

    router/organiztion.py ... working

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.models import BASE
 from app.database import engine
 from app.auths.auth import auths_router
+from app.routers.organizations import org_router
 
 # create instance 
 app = FastAPI(title="NexusAPI")
@@ -9,7 +10,7 @@ app = FastAPI(title="NexusAPI")
 BASE.metadata.create_all(bind=engine)
 # attach routers
 app.include_router(auths_router,prefix='/auth',tags=['auth'])
-
+app.include_router(org_router,prefix='/org',tags=['org'])
 
 @app.get('/')
 def root():
